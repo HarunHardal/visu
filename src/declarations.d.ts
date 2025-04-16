585ab5e1acf9972c684b071cf01478b56a41ef01
unknown <theredwolf26@gmail.com>
2025-04-16 13:59:45 +0300

    Güncellemeler yapıldı

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,4 +1,13 @@
 declare module '*.glsl' {
-    const value: string;
-    export default value;
+  const value: string;
+  export default value;
+}
+
+import { JSX } from 'react-jsx';
+import { elements } from '@react-three/fiber';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements extends elements { }
   }
+}
