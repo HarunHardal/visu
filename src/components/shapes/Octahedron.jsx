b695e701458cd63c1f6b4868d1a419a5928cc332
unknown <theredwolf26@gmail.com>
2025-05-05 16:35:59 +0300

    Güncellemeler yapıldı

--- src/components/shapes/Octahedron.jsx
+++ src/components/shapes/Octahedron.jsx
@@ -1,54 +1,3 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useThree, useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
-
-const OctahedronMesh = ({ scrollY }) => {
-    const meshRef = useRef(null);
-    const { scene } = useThree();
-    const [envMap, setEnvMap] = useState(null);
-
-    useEffect(() => {
-        new THREE.TextureLoader().load('/textures/last.jpg', (texture) => {
-            texture.mapping = THREE.EquirectangularReflectionMapping;
-            texture.colorSpace = THREE.SRGBColorSpace;
-            setEnvMap(texture);
-            scene.environment = texture;
-        });
-    }, [scene]);
-
-    useFrame(() => {
-        if (meshRef.current) {
-            const speed = 0.002;
-            meshRef.current.rotation.x = Math.sin(scrollY * speed) * 2;
-            meshRef.current.rotation.y = Math.cos(scrollY * speed) * 0.5;
-            meshRef.current.rotation.z = Math.sin(scrollY * speed) * 0.3;
-        }
-    });
-
-    return (
-        <>
-            <ambientLight intensity={1} />
-            <directionalLight position={[5, 5, 5]} intensity={1} />
-            <mesh ref={meshRef}>
-                <octahedronGeometry args={[1, 0]} />
-                <meshStandardMaterial
-                    color="#ffffff"
-                    metalness={1}
-                    roughness={0}
-                    envMap={envMap ?? undefined}
-                    envMapIntensity={1}
-                />
-            </mesh>
-        </>
-    );
-};
-
-export default OctahedronMesh;
-
-
-
-
-/*
 import React, { useEffect, useRef, useState, Suspense } from 'react';
 import { Canvas, useThree, useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
@@ -99,6 +48,9 @@ const OctahedronMesh = () => {
                     envMapIntensity={1}
                 />
             </mesh>
+            <Suspense>
+                <Environment files="/textures/uploads_files_4110099_nebula-1.hdr" background={false} />
+            </Suspense>
         </>
     );
 };
@@ -116,4 +68,3 @@ const Octahedron = () => {
 };
 
 export default Octahedron;
-*/
